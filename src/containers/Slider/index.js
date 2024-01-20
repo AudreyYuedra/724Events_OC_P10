1b68d1afc4f68e82f4c2ec58355ca3537dcf88af
Audrey Luquet <audrey.luquet@tutanota.com>
2024-01-20 16:09:21 +0100

    (slider) affichage date ordre décroissant

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,54 +1,50 @@
-import { useEffect, useState } from "react";
-import { useData } from "../../contexts/DataContext";
-import { getMonth } from "../../helpers/Date";
+import { useEffect, useState } from "react"
+import { useData } from "../../contexts/DataContext"
+import { getMonth } from "../../helpers/Date"
 
-import "./style.scss";
+import "./style.scss"
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
+   const { data } = useData()
+   const [index, setIndex] = useState(0)
 
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
-  );
+   //* Ordre d'affichage en décroissant
+   const byDateDesc = data?.focus.sort((evtA, evtB) => (new Date(evtA.date) > new Date(evtB.date) ? -1 : 1))
 
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+   const nextCard = () => {
+      setTimeout(() => setIndex(index < 2 ? index + 1 : byDateDesc.length - 3), 5000)
+   }
 
-  useEffect(() => {
-    nextCard();
-  });
+   useEffect(() => {
+      nextCard()
+   })
 
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div key={event.title} className={`SlideCard SlideCard--${ index === idx ? "display" : "hide"}`} >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
+   return (
+      <div className="SlideCardList">
+         {byDateDesc?.map((event, idx) => (
+            <>
+               <div key={event.title} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+                  <img src={event.cover} alt="forum" />
+                  <div className="SlideCard__descriptionContainer">
+                     <div className="SlideCard__description">
+                        <h3>{event.title}</h3>
+                        <p>{event.description}</p>
+                        <div>{getMonth(new Date(event.date))}</div>
+                     </div>
+                  </div>
+               </div>
 
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input key={`${event.id}`} type="radio" name="radio-button" checked={idx === radioIdx} />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
-};
+               <div className="SlideCard__paginationContainer">
+                  <div className="SlideCard__pagination">
+                     {byDateDesc.map((_, radioIdx) => (
+                        <input key={`${event.id}`} type="radio" name="radio-button" checked={idx === radioIdx} />
+                     ))}
+                  </div>
+               </div>
+            </>
+         ))}
+      </div>
+   )
+}
 
-export default Slider;
+export default Slider

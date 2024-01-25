009f83e8e7f33d0ad5c2b2dbfc017b4f595bbe87
Audrey Luquet <audrey.luquet@tutanota.com>
2024-01-25 15:28:06 +0100

    (test) ajout test Date

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,23 @@
-/**
- * 
- */
+import { getMonth } from "./index"
 
 describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
-    });
+   describe("When getMonth is called", () => {
+      // test vérif fonction date = janvier
+      it("the function return janvier for 2022-01-01 as date", () => {
+         // Creation new date janvier
+         const date = new Date("2022-01-01")
+         // Appel fonction avec new date crée
+         const result = getMonth(date)
+         // Vérif new date = janvier
+         expect(result).toBe.apply("janvier")
+      })
+      // test vérif fonction date = juillet
+      it("the function return juillet for 2022-07-08 as date", () => {
+         // Création new date juillet
+         const date = new Date("2022-07-08")
+         // Appel fonction avec new date crée
+         const résult = getMonth(date)
+         // Vérif new date = juillet
+      })
+   })
 })
-
